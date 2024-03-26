cf8f0662e8c0a9150740f4ac6cd327d985c0b5bb
Lielly Nátally Germano Ferreira <20211011110023@ifrn.local>
2024-03-26 18:02:37 -0300

    avaliacao-pascoa-inicial

--- src/App.tsx
+++ src/App.tsx
@@ -44,17 +44,136 @@ const ItemTarefa = (props: {titulo: string}) => {
   return (<li>{props.titulo}</li>);
 }
 
+type Publicacao = {
+  id: number,
+  titulo: string,
+  body: string;
+};
+
 const ListaDePublicacoes = () => {
-  return null;
+  const [publicacoes, setPublicacoes] = useState([]);
+  const escutarCliqueAcessarAPI = () => {
+    axios.get("https://jsonplaceholder.typicode.com/posts/").then((resposta: AxiosResponse) => {
+      const posts = resposta.data.map((post: { id: number; title: string; body: string; }) => {
+        return {
+          id: post.id,
+          titulo: post.title,
+          conteudo: post.body,
+        };
+      });
+      setPublicacoes(posts);
+    });
+  };  
+  return (
+    <>
+      <h4>Publicacoes</h4>
+      <div>
+        <button onClick={escutarCliqueAcessarAPI}>Atualizar publicações</button>
+      </div>
+      <ul>
+        {
+          publicacoes.map((item: Publicacao) => {
+            return <ItemPublicacao key={item.id} titulo={item.titulo} body={item.body} />
+          })
+        }
+      </ul>
+    </>
+  );
 }
+const ItemPublicacao = (props: {titulo: string}) => {
+  return (<li>{props.titulo}</li>
+  <li>{props.titulo}</li>
+  <li>{props.titulo}</li>);
+}
+
+type Usuario = {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  address: object,
+  phone: string,
+  website: string,
+  company: object;
+};
 
 const ListaDeUsuarios = () => {
-  return null;
+  const [usuarios, setUsuarios] = useState([]);
+  const escutarCliqueAcessarAPI = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users/").then((resposta: AxiosResponse) => {
+      const users = resposta.data.map((user: { id: number; name: string; username: string; email: string;  address: object; phone: string; website: string; company: object;}) => {
+        return {
+          id: user.id,
+          nome: user.name,
+          usuario: user.username,
+          email: user.email,
+          endereco: user.address,
+          telefone: user.phone,
+          site: user.website,
+          companhia: user.company,
+        };
+      });
+      setUsuarios(users);
+    });
+  };  
+  return (
+    <>
+      <h4>Publicacoes</h4>
+      <div>
+        <button onClick={escutarCliqueAcessarAPI}>Atualizar publicações</button>
+      </div>
+      <ul>
+        {
+          usuarios.map((item: Usuario) => {
+            return <ItemUsuario key={item.id} titulo={} />
+          })
+        }
+      </ul>
+    </>
+  );
+}
+const ItemUsuario = (props: {titulo: string}) => {
+  return (<li>{props.titulo}</li>);
 }
 
+type Album = {
+  id: number,
+  titulo: string;
+};
+
 const ListaDeAlbuns = () => {
-  return null;
+  const [albuns, setAlbuns] = useState([]);
+  const escutarCliqueAcessarAPI = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users/").then((resposta: AxiosResponse) => {
+      const albuns = resposta.data.map((album: { id: number; title: string;}) => {
+        return {
+          id: album.id,
+          titulo: album.title
+        };
+      });
+      setAlbuns(albuns);
+    });
+  };  
+  return (
+    <>
+      <h4>Albuns</h4>
+      <div>
+        <button onClick={escutarCliqueAcessarAPI}>Atualizar albuns</button>
+      </div>
+      <ul>
+        {
+          albuns.map((item: Album) => {
+            return <ItemAlbum key={item.id} titulo={item.titulo} />
+          })
+        }
+      </ul>
+    </>
+  );
 }
+const ItemAlbum = (props: {titulo: string}) => {
+  return (<li>{props.titulo}</li>);
+}
+
 
 const App = () => {
   return (
